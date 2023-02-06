5ca5469805ce083777720c9a98305be4f41329e7
eriksturen <eriksturen@gmail.com>
2023-02-06 09:52:56 +0100

    Modal working again!

--- site.js
+++ site.js
@@ -11,7 +11,7 @@ class Product {
 
 const productList = document.querySelector("#productList")
 const products = [];
-const productModal = document.querySelector(".product-info-modal");
+const productModal = document.querySelector("#productModal");
 
 
 async function startShop() {
@@ -61,6 +61,7 @@ function createProductCards() {
         // Set up events for buttons
         modalButton.onclick = () => {
             createModal(product);
+            $('#exampleModal').modal('show');
         };
 
         cartButton.onclick = () => {
@@ -75,88 +76,61 @@ function createProductCards() {
     }
 }
 
-// function createModal(product) {
-//     const productModal = document.querySelector("#productModal");
-//     const modal = document.createElement("div");
-//     const modalDialog = document.createElement("div");
-//     const modalContent = document.createElement("div");
-//     const modalHeader = document.createElement("div");
-//     const modalTitle = document.createElement("h5");
-//     const modalBody = document.createElement("div");
-//     const modalFooter = document.createElement("div");
-//     const closeButton = document.createElement("button");
-//     const cartButton = document.createElement("button");
-
-//     const modalBodyTitle = document.createElement("p");
-//     const modalBodyDescription = document.createElement("p");
-//     const modalBodyPrice = document.createElement("p");
-
-
-//     modal.classList.add("modal");
-//     // modal.role = "dialog";
-//     // modal.tabIndex = "-1";
-//     // modal.ariaHidden = "true";
-//     modalDialog.classList.add("modal-dialog");
-//     modalDialog.role = "document";
-//     modalContent.classList.add("modal-content");
-//     modalHeader.classList.add("modal-header");
-//     modalTitle.classList.add("modal-title");
-//     modalBody.classList.add("modal-body");
-//     modalFooter.classList.add("modal-footer");
-//     closeButton.classList.add("close", "btn", "btn-primary");
-//     cartButton.classList.add("close", "btn", "btn-primary");
-
-//     closeButton.onclick = () => {
-//         $('#myModal').modal('hide');
-//     }
-
-//     modalTitle.innerText = product.title;
-//     modalBodyTitle.innerText = product.title;
-//     modalBodyDescription.innerText = product.description;
-//     modalBodyPrice.innerText = product.price;
-//     closeButton.innerText = "Stäng";
-//     cartButton.innerText = "Lägg i korgen";
-
-//     modalBody.append(modalBodyTitle, modalBodyDescription, modalBodyPrice);
-//     modalFooter.append(closeButton, cartButton);
-//     modalHeader.append(modalTitle);
-//     modalContent.append(modalHeader, modalBody, modalFooter);
-//     modalDialog.append(modalContent);
-//     modal.append(modalDialog);
-
-//     console.log(modal.innerHTML);
-//     productModal.firstChild = modal;
-// }
-
-// TODO change this to similar to above - take in product instead 
 function createModal(product) {
-    // The doubleclick problem on the product modal was becasue the product list was read again in that function. It's unnecessary because it's already stored in the local storage - when productlist is gotten from there instead it works on first click
-    productModal.innerHTML = `
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">${product.title}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>${product.title}</p>
-                    <p>${product.description}</p>
-                    <p>${product.price}</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Stäng</button>
-                    <button type="button" class="btn btn-primary">Lägg i korgen</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `
-    console.log(productModal.innerHTML);
+    const productModal = document.querySelector("#productModal");
+    const modal = document.createElement("div");
+    const modalDialog = document.createElement("div");
+    const modalContent = document.createElement("div");
+    const modalHeader = document.createElement("div");
+    const modalTitle = document.createElement("h5");
+    const modalBody = document.createElement("div");
+    const modalFooter = document.createElement("div");
+    const closeButton = document.createElement("button");
+    const cartButton = document.createElement("button");
+
+    const modalBodyTitle = document.createElement("p");
+    const modalBodyDescription = document.createElement("p");
+    const modalBodyPrice = document.createElement("p");
+
+
+    modal.classList.add('modal');
+    modal.role = "dialog";
+    modal.tabIndex = "-1";
+    modal.ariaHidden = "true";
+    modalDialog.classList.add("modal-dialog");
+    modalDialog.role = "document";
+    modalContent.classList.add("modal-content");
+    modalHeader.classList.add("modal-header");
+    modalTitle.classList.add("modal-title");
+    modalBody.classList.add("modal-body");
+    modalFooter.classList.add("modal-footer");
+    closeButton.classList.add("close", "btn", "btn-primary");
+    cartButton.classList.add("close", "btn", "btn-primary");
+
+    closeButton.onclick = () => {
+        $('#exampleModal').modal('hide');
+    }
+
+    modalTitle.innerText = product.title;
+    modalBodyTitle.innerText = product.title;
+    modalBodyDescription.innerText = product.description;
+    modalBodyPrice.innerText = product.price;
+    closeButton.innerText = "Stäng";
+    cartButton.innerText = "Lägg i korgen";
+
+    modalBody.append(modalBodyTitle, modalBodyDescription, modalBodyPrice);
+    modalFooter.append(closeButton, cartButton);
+    modalHeader.append(modalTitle);
+    modalContent.append(modalHeader, modalBody, modalFooter);
+    modalDialog.append(modalContent);
+    modal.appendChild(modalDialog);
+
+
+    modal.id = "exampleModal";
+    console.log(modal);
+    productModal.appendChild(modal);
 }
+
 
 // local storage class 
 class Storage {
@@ -172,18 +146,33 @@ class Storage {
 startShop();
 
 
-// {/* <div class="modal-dialog">
-//     <div class="modal-content">
-//         <div class="modal-header"><h5 class="modal-title">Köttfärs på tunna</h5>
-//         </div>
-//         <div class="modal-body">
-//             <p>Köttfärs på tunna</p>
-//             <p>Stor tunna med prima köttfärs. Mängdrabatt!</p>
-//             <p>10.99</p>
-//         </div>
-//         <div class="modal-footer">
-//             <button class="close btn btn-primary">Stäng</button>
-//             <button class="close btn btn-primary">Lägg i korgen</button>
+// OLD MODAL FUNCTION
+// TODO change this to similar to above - take in product instead
+// function createModal(product) {
+//     // The doubleclick problem on the product modal was becasue the product list was read again in that function. It's unnecessary because it's already stored in the local storage - when productlist is gotten from there instead it works on first click
+//     productModal.innerHTML = `
+//         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+//         aria-hidden="true">
+//         <div class="modal-dialog" role="document">
+//             <div class="modal-content">
+//                 <div class="modal-header">
+//                     <h5 class="modal-title" id="exampleModalLabel">${product.title}</h5>
+//                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//                         <span aria-hidden="true">&times;</span>
+//                     </button>
+//                 </div>
+//                 <div class="modal-body">
+//                     <p>${product.title}</p>
+//                     <p>${product.description}</p>
+//                     <p>${product.price}</p>
+//                 </div>
+//                 <div class="modal-footer">
+//                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Stäng</button>
+//                     <button type="button" class="btn btn-primary">Lägg i korgen</button>
+//                 </div>
+//             </div>
 //         </div>
 //     </div>
-// </div> */}
+//     `
+//     console.log(productModal.innerHTML);
+// }
