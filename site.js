132ef95d7ee43f8573cd2d167dd1936c1108153a
eriksturen <eriksturen@gmail.com>
2023-02-06 18:13:11 +0100

    Cart working, with adding and removing products

--- site.js
+++ site.js
@@ -7,17 +7,15 @@ class Product {
         this.image = image;
     }
 }
-// this is main cart variable
 
 const productList = document.querySelector("#productList")
 const products = [];
-const productModal = document.querySelector("#productModal");
+// const productModal = document.querySelector("#productModal");
 
 
 async function startShop() {
     const response = await fetch("./products/products.json");
     const productsJson = await response.json();
-    console.log(productsJson.items)
     for (const product of productsJson.items) {
         products.push(
             new Product(product.title, product.price, product.description, product.image)
@@ -26,9 +24,123 @@ async function startShop() {
     createProductCards();
 }
 
-let cart = [];
-//buttons 
+function openCart() {
+    cartModal();
+    $('#cartModal').modal('show');
+    console.log("print cart")
+}
 
+// Look in localStorage - if cart is there, load that one, otherwise initialize
+// this is main cart variable
+let cart = [];
+function cartModal() {
+    const cartModal = document.querySelector("#shopping-cart-modal");
+    // Clear old modal if it's there
+    cartModal.firstChild.remove();
+
+    let totalPrice = 0;
+
+    // need a cartModal with title "Kundkorg". 
+    const modal = document.createElement("div");
+    const modalDialog = document.createElement("div");
+    const modalContent = document.createElement("div");
+    const modalHeader = document.createElement("div");
+    const modalTitle = document.createElement("h5");
+    const modalBody = document.createElement("div");
+    const modalCardList = document.createElement("ul");
+    const modalFooter = document.createElement("div");
+    const totalPriceDiv = document.createElement("div");
+    const closeCartButton = document.createElement("button");
+    const emptyCartButton = document.createElement("button");
+    const checkoutButton = document.createElement("button");
+
+
+    // add classes
+    modal.classList.add('modal');
+    modal.role = "dialog";
+    modal.tabIndex = "-1";
+    modal.ariaHidden = "true";
+    modalDialog.classList.add("modal-dialog");
+    modalDialog.role = "document";
+    modalContent.classList.add("modal-content");
+    modalHeader.classList.add("modal-header");
+    modalTitle.classList.add("modal-title");
+    modalBody.classList.add("modal-body");
+    modalFooter.classList.add("modal-footer");
+    closeCartButton.classList.add("close", "btn", "btn-primary");
+    emptyCartButton.classList.add("btn", "btn-danger");
+    checkoutButton.classList.add("btn", "btn-success");
+
+
+    //Then a list of small cards - horizontally aligned with products. Title, Price, amount, lower/increase buttons, total price for each of item and remove button
+    for (const product of cart) {
+        const productContainer = document.createElement("div");
+        const productRow = document.createElement("div")
+        const productText = document.createElement("div");
+        const productAmount = document.createElement("div");
+        const productPrice = document.createElement("div");
+        const removeProduct = document.createElement("div");
+
+        productContainer.classList.add("container");
+        productRow.classList.add("row");
+        productRow.style.border = "1px solid grey";
+
+        productText.classList.add("col-4", "m-3");
+        productPrice.classList.add("col", "m-3");
+        productAmount.classList.add("col");
+        removeProduct.classList.add("col", "btn", "btn-danger", "m-3");
+        removeProduct.style.maxHeight = "40px";
+
+        productText.innerText = product.title;
+        productPrice.innerText = `à ${product.price} kr`
+        removeProduct.innerText = "Ta bort";
+
+        // remove product clicky thing doesn't work
+        removeProduct.onclick = () => {
+            const index = cart.indexOf(product);
+            cart.splice(index, 1);
+            $("#cartModal").modal("hide");
+            openCart();
+        }
+
+        // Append everything to the main list
+        productRow.append(productText, productPrice, removeProduct);
+        productContainer.append(productRow);
+        modalCardList.append(productContainer);
+
+        totalPrice += product.price;
+    }
+    // then a footer with total price, empty cart button and checkout button
+
+    modalTitle.innerText = "Kundkorg";
+    totalPriceDiv.innerText = `${totalPrice} kr`;
+    closeCartButton.innerText = "Stäng";
+    emptyCartButton.innerText = "Töm korgen";
+    checkoutButton.innerText = "Gå till kassan";
+
+    // functionality for cart buttons
+    emptyCartButton.onclick = () => {
+        cart.splice(0, cart.length);
+        $("#cartModal").modal("hide");
+        openCart();
+    }
+
+    closeCartButton.onclick = () => {
+        $("#cartModal").modal("hide");
+    }
+
+    // append everything to the main Modal
+    modalFooter.append(totalPriceDiv, emptyCartButton, checkoutButton);
+    modalBody.append(modalCardList, modalFooter);
+    modalHeader.append(modalTitle, closeCartButton);
+    modalContent.append(modalHeader, modalBody, modalFooter);
+    modalDialog.append(modalContent);
+    modal.appendChild(modalDialog);
+
+    modal.id = "cartModal";
+    cartModal.appendChild(modal);
+    console.log(cartModal.innerHTML);
+}
 
 function createProductCards() {
     for (const product of products) {
@@ -54,7 +166,7 @@ function createProductCards() {
 
         cardTitle.innerText = product.title;
         cardImage.src = product.image;
-        cardText.innerText = product.price;
+        cardText.innerText = `${product.price} kr`;
         modalButton.innerText = "Mer info";
         cartButton.innerText = "Lägg i korgen";
 
@@ -113,10 +225,15 @@ function createModal(product) {
         $('#exampleModal').modal('hide');
     }
 
+    // Cart button needs to go here 
+    cartButton.onclick = () => {
+        addToCart(product);
+    }
+
     modalTitle.innerText = product.title;
     modalBodyTitle.innerText = product.title;
     modalBodyDescription.innerText = product.description;
-    modalBodyPrice.innerText = product.price;
+    modalBodyPrice.innerText = `${product.price} kr`;
     closeButton.innerText = "Stäng";
     cartButton.innerText = "Lägg i korgen";
 
@@ -129,8 +246,13 @@ function createModal(product) {
 
 
     modal.id = "exampleModal";
-    console.log(modal);
     productModal.appendChild(modal);
+    console.log(productModal.innerHTML);
+}
+
+function addToCart(product) {
+    cart.push(product);
+    console.log(cart);
 }
 
 
